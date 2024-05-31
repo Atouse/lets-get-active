d73f71da54156ebe6f8a80c2414348d3eb6d7d76
Vic The Big Dick <sonicxvic23@gmail.com>
2024-05-31 16:37:17 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,43 @@ const References = () => {
   )
 }
 
+const Informations = (info) => {
+  return (
+    <div id="info">
+      <h3> {info.title}</h3>
+      <span> {info.desc}</span> 
+      <a href="#refs"> Reference</a>
+    </div>
+  )
 
+}
+
+const faxNoPrinter = {
+  "health": {
+    "title": "Health Benefits",
+    "desc": "The long term benefits for your health are better cholesterol levels, lower levels of stress, and longer life span"
+  },
+  "social":{
+
+  },
+  "economic":{
+
+  }
+}
 function App() {
   return (
     <div className="App">
-      <h1> Benefits of daily physical activity</h1>
-      <References></References>
+      <h1> Benefits of exercising</h1>
+
+
+
+      <Informations title={faxNoPrinter.health.title} desc={faxNoPrinter.health.desc}></Informations>
+
+
+
+
+
+      <References ></References>
     </div>
   );
 }
